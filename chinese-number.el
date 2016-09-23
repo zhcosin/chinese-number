d79bc4d50fe104ea60834404139e2a2f14101ee5
zhcosin <zhcosin@163.com>
2016-09-23 13:15:36 +0800

    adjust name for some function.

--- chinese-number.el
+++ chinese-number.el
@@ -22,41 +22,41 @@
   (cnum-get-chinese-number-and-weight-general number-index weight-index 'cnum-get-chinese-number 'cnum-get-chinese-weight))
 
 (defun cnum-get-chinese-number-and-weight-high (number weight-index)
-  (cnum-get-chinese-number-and-weight-general number weight-index 'cnum-chinese-small-number 'cnum-get-chinese-high-level-weight))
+  (cnum-get-chinese-number-and-weight-general number weight-index 'cnum-convert-to-chinese-for-number-less-than-10000 'cnum-get-chinese-high-level-weight))
 
 (defun cnum-get-chinese-number-weight (number weight-index disable-zero)
   (if (= number 0)
       (if disable-zero "" (get-chinese-number 0))
     (cnum-get-chinese-number-and-weight number weight-index)))
 
-(defun cnum-chinese-number-general-iter (number index next-is-zero base fun-to-convert-small-number iter-depth)
+(defun cnum-convert-to-chinese-general-iter (number index next-is-zero base fun-to-convert-small-number iter-depth)
   (let ((real-number (% number base)))
   (if (< number base)
       (funcall fun-to-convert-small-number number index (if (= iter-depth 0) nil next-is-zero))
-    (concat (cnum-chinese-number-general-iter (/ number base) (+ index 1) (= real-number 0) base fun-to-convert-small-number (+ iter-depth 1))
+    (concat (cnum-convert-to-chinese-general-iter (/ number base) (+ index 1) (= real-number 0) base fun-to-convert-small-number (+ iter-depth 1))
 	    (funcall fun-to-convert-small-number real-number index next-is-zero)))))
 
-(defun cnum-chinese-small-number-iter (number index next-is-zero)
-  (cnum-chinese-number-general-iter number index next-is-zero 10 'cnum-get-chinese-number-weight 0))
+(defun cnum-convert-to-chinese-for-number-less-than-10000-iter (number index next-is-zero)
+  (cnum-convert-to-chinese-general-iter number index next-is-zero 10 'cnum-get-chinese-number-weight 0))
 
-(defun cnum-chinese-high-number-iter (number index next-is-zero)
-  (cnum-chinese-number-general-iter number index next-is-zero 10000 'cnum-get-chinese-number-and-weight-high 0)
+(defun cnum-convert-to-chinese-for-number-iter (number index next-is-zero)
+  (cnum-convert-to-chinese-general-iter number index next-is-zero 10000 'cnum-get-chinese-number-and-weight-high 0)
   )
 
-(defun cnum-chinese-small-number (number)
-  (cnum-chinese-small-number-iter number 0 t))
+(defun cnum-convert-to-chinese-for-number-less-than-10000 (number)
+  (cnum-convert-to-chinese-for-number-less-than-10000-iter number 0 t))
 
-(defun cnum-chinese-high-number (number)
-  (cnum-chinese-high-number-iter number 0 t))
+(defun cnum-convert-to-chinese-for-number (number)
+  (cnum-convert-to-chinese-for-number-iter number 0 t))
 
 (defun cnum-convert-number-to-chinese (number)
   "convert a number from Arabic to Chinese format."
   (interactive "nInput the Arabic number: ")
-  (message "The chinese format for number %d is %s" number (cnum-chinese-high-number number)))
+  (message "The chinese format for number %d is %s" number (cnum-convert-to-chinese-for-number number)))
 
 ;; for test
-;;(convert-number-to-chinese 0)
-;;(convert-number-to-chinese 1230)
-;;(convert-number-to-chinese 380070500)
+(convert-number-to-chinese 0)
+(convert-number-to-chinese 1230)
+(convert-number-to-chinese 380070500)
 
 (provide 'chinese-number)
